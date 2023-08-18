296c2b0e6bcfa723f97d22738b077adab7fe2c06
bpsmuna <142607047+bpsmuna@users.noreply.github.com>
2023-08-18 22:38:43 +0800

    Add files via upload

---
+++ assets/data/JSONData.js
@@ -0,0 +1,155 @@
+var soals = {
+    "items": [
+        {
+            "quest": "1. Layanan pada smartphone yang memungkinkan pengguna untuk dapat mencari dan menemukan informasi di internet disebut:",
+            "name" : "entry.1432902502",
+            "opts": [
+                "A. Browser",
+                "B. Playstore",
+                "C. Email",
+                "D. VPN"
+            ]
+        },
+        {
+            "quest": "2. Sistem operasi yang disematkan pada smartphone android adalah:",
+            "name" : "entry.987599292",
+            "opts": [
+                "A. iOS",
+                "B. Android",
+                "C. Windows",
+                "D. Linux"
+            ]
+        },
+        {
+            "quest": "3. Komponen utama pada smartphone yang berfungsi sebagai pusat pemrosessan segala aktivitas pada smartphone, kecuali:",
+            "name" : "entry.508561545",
+            "opts": [
+                "A. CPU",
+                "B. GPU",
+                "C. RAM",
+                "D. Harddisk"
+            ]
+        },
+        {
+            "quest": "4. Fitur yang ditujukan untuk mengetahui posisi suatu wilayah/tempat pada smartphone disebut:",
+            "name" : "entry.1568709206",
+            "opts": [
+                "A. GPS",
+                "B. NFC",
+                "C. Wifi",
+                "D. Mobile Data"
+            ]
+        },
+        {
+            "quest": "5. Layanan yang digunakan untuk menginstall aplikasi secara legal pada smartphone tipe android adalah",
+            "name" : "entry.1280009712",
+            "opts": [
+                "A. Playstore",
+                "B. AppStore",
+                "C. Microsoft Store",
+                "D. Steam"
+            ]
+        },
+        {
+            "quest": "6. Berikut adalah layanan Google yang sering digunakan untuk mencari informasi di internet, kecuali:",
+            "name" : "entry.739910104",
+            "opts": [
+                "A. Chrome",
+                "B. Zoom",
+                "C. Whatsapp",
+                "D. Spotify"
+            ]
+        },
+        {
+            "quest": "7. Contoh aplikasi berikut ini yang digunakan sebagai sarana komunikasi adalah:",
+            "name" : "entry.1117785666",
+            "opts": [
+                "A. Opera Mini",
+                "B. Whatsapp",
+                "C. VPN",
+                "D. Microsoft Office"
+            ]
+        },
+        {
+            "quest": "8. Perangkat pada smartphone yang bertugas untuk mengambil gambar adalah:",
+            "name" : "entry.736817138",
+            "opts": [
+                "A. Kamera",
+                "B. Browser",
+                "C. Prosessor",
+                "D. Speaker"
+            ]
+        },
+        {
+            "quest": "9. Berikut ini merupakan fungsi dari layanan internet, kecuali:",
+            "name" : "entry.570458667",
+            "opts": [
+                "A. Sarana tempat memperoleh dan bertukar informasi",
+                "B. Sarana tempat berkomunikasi",
+                "C. Sarana bisnis dan hiburan",
+                "D. Alat pembayaran"
+            ]
+        },
+        {
+            "quest": "10. Perintah shutdown pada smartphone digunakan untuk:",
+            "name" : "entry.288824105",
+            "opts": [
+                "A. Menghidupkan ulang smartphone",
+                "B. Menonaktifkan smartphone",
+                "C. Mengidupkan dan menonaktifkan smartphone",
+                "D. Mode pesawat terbang “Air Plane”"
+            ]
+        },
+        {
+            "quest": "11. Aplikasi/program yang sering digunakan BPS untuk memperoleh koordinat letak titik bangunan disebut:",
+            "name" : "entry.2144473965",
+            "opts": [
+                "A. FortiClient",
+                "B. Wilkerstat",
+                "C. Kaizala",
+                "D. Microsoft Office"
+            ]
+        },
+        {
+            "quest": "12. Manakah perangkat smartphone berikut yang berfungsi agar pengguna tetap terhubung dengan internet, kecuali:",
+            "name" : "entry.879722091",
+            "opts": [
+                "A. Mobile Data /Paket Data",
+                "B. Wifi",
+                "C. Hospot/Tethering",
+                "D. NFC"
+            ]
+        },
+        {
+            "quest": "13. Perangkat yang digunakan untuk mengetik pada layer smartphone disebut:",
+            "name" : "entry.1170136059",
+            "opts": [
+                "A. Printer",
+                "B. Mouse",
+                "C. Keyboard",
+                "D. Prosessor"
+            ]
+        },
+        {
+            "quest": "14. RAM pada smartphone berfungsi untuk menentukan:",
+            "name" : "entry.1489489908",
+            "opts": [
+                "A. Kecepatan pemrosessan data",
+                "B. Kecepatan ber-internet",
+                "C. Kecepatan menyimpan data",
+                "D. Kecepatan mengambil foto/video"
+            ]
+        },
+        {
+            "quest": "15. Bagian yang berfungsi untuk menampilkan hasil pemrosessan data pada smartphone disebut:",
+            "name" : "entry.234288250",
+            "opts": [
+                "A. Kamera",
+                "B. Speaker",
+                "C. Panel layer",
+                "D. Browser"
+            ]
+        },
+        
+    ]
+}
